731b2cf0098ce3e704b07e366fb12c4d0c22f761
jianshao <wujianshao@gmail.com>
2011-05-19 16:25:34 +0800

    add exception catch

--- src/place/api/views.py
+++ src/place/api/views.py
@@ -1,4 +1,3 @@
-
 from django import forms
 from orange.django.place.utils import get_json_response
 from orange.place import errors
@@ -14,12 +13,18 @@ def internal_method(request):
         in_form = InternalForm(request.GET)
         logger.debug(in_form.data)
         if in_form.is_valid():
-            method = getattr(place.api.methods, in_form.cleaned_data[MethodConsts.METHOD])
-            return method(request)
+            try:
+                method = getattr(place.api.methods, in_form.cleaned_data[MethodConsts.METHOD])
+                return method(request)
+            except Exception:
+                returnCode = errors.INTERNAL_ERROR
+                return get_json_response(get_return_dict(returnCode, message='unknown error'))
         else:
             returnCode = errors.ERROR_PARAMETER
             return get_json_response(get_return_dict(returnCode, message=in_form.errors))
-    logger.warn('internal method should not be posted')
+    else:
+        returnCode = errors.ERROR_PARAMETER
+        return get_json_response(get_return_dict(returnCode, message='not GET method'))
 
 class InternalForm(forms.Form):
     m = forms.CharField(max_length=10)
